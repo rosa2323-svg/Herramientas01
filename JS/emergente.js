a814d778f7dabf0af6a0f75785031c1ea14cebd4
MaelXD <fiestasjheins@gmail.com>
2026-09-07 17:18:24 -0500

    feat: implementar popup promocional con localStorage
    Se configuro el popup para aparecer a los 3 segundos en el Index.
    Se integro localStorage para guardar el estado y evitar que vuelva a salir si el usuario ya lo cerro.

--- JS/emergente.js
+++ JS/emergente.js
@@ -1,3 +1,106 @@
+
+//  MODAL PROMOCIÓN EMERGENTE
+const elementoModal = document.getElementById('promoModal');
+const hayLoginError = document.getElementById('loginError') !== null;
+
+if (elementoModal && !hayLoginError) {
+    const myModal = new bootstrap.Modal(elementoModal);
+    setTimeout(() => {
+        myModal.show();
+    }, 1000);
+}
+
+// MODAL LOGIN CON ERROR
+const loginModal = document.getElementById('loginModal');
+if (loginModal && hayLoginError) {
+    const modal = new bootstrap.Modal(loginModal);
+    modal.show();
+}
+
+// MODAL REGISTRO EXITOSO
+const registroExitosoModal = document.getElementById('registroExitosoModal');
+if (registroExitosoModal && registroExitosoModal.dataset.show === 'true') {
+    const modal = new bootstrap.Modal(registroExitosoModal);
+    modal.show();
+}
+
+// Barra de Busqueda
+const input = document.getElementById('inputBusqueda');
+const suggestionsBox = document.getElementById('suggestionsBox');
+const productGrid = document.getElementById('productGrid');
+
+if (input) {
+    input.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            const term = this.value.trim();
+            if (term) {
+                window.location.href = window.location.origin + '/buscar?query=' + encodeURIComponent(term);
+            }
+        }
+    });
+
+    input.addEventListener('input', async function() {
+        const term = this.value.trim();
+        if (term.length < 1) {
+            suggestionsBox.classList.add('d-none');
+            return;
+        }
+        try {
+            const response = await fetch(`/api/v1/productos/keywords?term=${encodeURIComponent(term)}`);
+            const keywords = await response.json();
+            suggestionsBox.innerHTML = '';
+
+            if (keywords.length > 0) {
+                keywords.forEach(word => {
+                    const item = document.createElement('div');
+
+                    item.className = 'p-2 suggestion-item d-block text-dark';
+                    item.style.cursor = 'pointer';
+                    item.textContent = word;
+
+                    item.addEventListener('mousedown', function(eventoDeClic) {
+                        eventoDeClic.preventDefault();
+                        eventoDeClic.stopPropagation();
+
+                        const texto = word.toLowerCase().trim();
+                        const categorias = ["consolas", "juegos", "perifericos", "tarjetas", "sillas"];
+
+                        if (categorias.includes(texto)) {
+                            window.location.assign(window.location.origin + `/categoria/${texto}`);
+                        } else {
+                            window.location.assign(window.location.origin + `/buscar?query=${encodeURIComponent(word)}`);
+                        }
+                    }, true);
+
+                    suggestionsBox.appendChild(item);
+                });
+                suggestionsBox.classList.remove('d-none');
+            } else {
+                suggestionsBox.classList.add('d-none');
+            }
+        } catch (error) { console.error('Error:', error); }
+    });
+
+    document.addEventListener('click', function(e) {
+        const clickDentroDelInput = input.contains(e.target);
+        const clickDentroDeSugerencias = suggestionsBox.contains(e.target);
+
+        if (!clickDentroDelInput && !clickDentroDeSugerencias) {
+            suggestionsBox.classList.add('d-none');
+        }
+    });
+}
+
+// MODAL COMPRA
+const compraModal = document.getElementById('compraModal');
+if (compraModal) {
+    compraModal.addEventListener('show.bs.modal', function(event) {
+        const boton = event.relatedTarget;
+        document.getElementById('modalProductoNombre').textContent = boton.getAttribute('data-producto');
+        document.getElementById('modalProductoPrecio').textContent = boton.getAttribute('data-precio');
+    });
+}
 // MOSTRAR/OCULTAR CONTRASEÑA
 document.querySelectorAll('[data-toggle-password]').forEach(btn => {
     btn.addEventListener('click', function() {
